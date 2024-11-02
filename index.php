ca295f0b006bac20ab5b1ff560c5f62ed8009d42
luixh03 <enodepro3@gmail.com>
2024-11-02 10:37:19 -0500

    subido correctamente

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+
+require_once "Controladores/plantillaC.php";
+
+require_once "Controladores/secretariasC.php";
+require_once "Modelos/secretariasM.php";
+
+
+require_once "Controladores/consultoriosC.php";
+require_once "Modelos/consultoriosM.php";
+
+
+$plantilla = new Plantilla();
+$plantilla -> LlamarPlantilla();
